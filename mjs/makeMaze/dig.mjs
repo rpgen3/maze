4ec2a7945ca7e5ef32e955d2a09047b28d5d3f64
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-10-14 00:57:33 +0900

    Update dig.mjs

--- mjs/makeMaze/dig.mjs
+++ mjs/makeMaze/dig.mjs
@@ -1,6 +1,10 @@
 import {randInt, randArr} from 'https://rpgen3.github.io/mylib/export/random.mjs';
 const rand = arr => arr[Math.random() * arr.length | 0];
 export const dig = async ({width, height, update, updateAll, start = [1, 1]}) => {
+    { // x, yともに奇数となる座標を穴掘り開始座標にする
+        const [x, y] = start;
+        if(!(x % 2 && y % 2)) throw 'Start must be odd.';
+    }
     const toI = (x, y) => x + y * width;
     const toXY = i => {
         const x = i % width,
@@ -24,9 +28,7 @@ export const dig = async ({width, height, update, updateAll, start = [1, 1]}) =>
         }
     }
     await updateAll(maze);
-    // x, yともに奇数となる座標を穴掘り開始座標にする
-    for(const [i, v] of start.entries()) if(v % 2 === 0) start[i]++;
-    const road = [start]; // 既に2000年前に通過した道
+    const road = [toI(...start)]; // 既に2000年前に通過した道
     let unpaved = (width - 1) * (height - 1) / 4 - 1; // 未開の地
     const main = async () => {
         if(!unpaved) { // すべての処理の終わり
@@ -45,14 +47,12 @@ export const dig = async ({width, height, update, updateAll, start = [1, 1]}) =>
             return maze;
         }
         const idx = randInt(0, road.length - 1),
-              xy = toXY(road[idx]);
+              _i = road[idx];
         road.splice(idx, 1);
-        return extend(xy);
+        return extend(...toXY(_i));
     };
-    const now = []; // 現在拡張中の壁
-    const extend = async ([x, y]) => { // 壁延ばし本処理
+    const extend = async (x, y) => { // 壁延ばし本処理
         const _i = toI(x, y);
-        now.push(_i);
         await put(_i);
         const nexts = [
             [2, 0],
@@ -62,11 +62,11 @@ export const dig = async ({width, height, update, updateAll, start = [1, 1]}) =>
         ].map(([_x, _y]) => [_x + x, _y + y]).filter(([x, y]) => (x >= 0 && x < width) && (y >= 0 && y < height) && maze[toI(x, y)]);
         if(!nexts.length) return main(); // 四方がすべて現在拡張中の通路の場合
         else {
-            if(nexts.length > 1) road.push(toI(x, y));
+            if(nexts.length > 1) road.push(_i);
             const next = randArr(nexts);
             await put(toI(...[x, y].map((v, i) => v + (next[i] - v >> 1)))); // 奇数マス
             unpaved--;
-            return extend(next);
+            return extend(...next);
         }
     }
     return main();
