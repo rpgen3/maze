ec684b49cd46ca291d4383b3433a1ff45c2f193b
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-10-14 21:28:26 +0900

    Update dfs.mjs

--- mjs/search/dfs.mjs
+++ mjs/search/dfs.mjs
@@ -5,40 +5,56 @@ export const dfs = async ({maze, start, goal, width, height, update}) => {
               y = i / width | 0;
         return [x, y];
     };
-    const done = [...Array(width * height).fill(false)]; // 既に通った
     const getAbled = i => {
-        const [_x, _y] = toXY(i);
-        return [
-            [1, 0],
-            [-1, 0],
-            [0, 1],
-            [0, -1]
-        ].flatMap(([a, b]) => {
-            const [x, y] = [_x + a, _y + b];
-            if(x < 0 || x >= width || y < 0 || y >= height) return [];
-            const _i = toI(x, y);
-            return maze[_i] || done[_i] || stack.includes(_i) ? [] : [_i];
+        const [x, y] = toXY(i);
+        const way = [];
+        if(x !== 0) way.push([-1, 0]);
+        if(x !== width - 1) way.push([1, 0]);
+        if(y !== 0) way.push([0, -1]);
+        if(y !== height - 1) way.push([0, 1]);
+        return way.flatMap(([_x, _y]) => {
+            const _i = toI(_x + x, _y + y);
+            return maze[_i] || mapRoad.has(_i) || stack.includes(_i) ? [] : [_i];
         });
     };
-    const result = [],
-          stack = [toI(...start)],
-          _goal = toI(...goal);
+    const _start = toI(...start),
+          _goal = toI(...goal),
+          stack = [_start];
+    let node = null,
+        found = false;
+    mapRoad.clear();
     while(stack.length) {
         const _i = stack.pop();
         await update(_i);
-        if(_i === _goal) break;
-        done[_i] = true;
+        if(_i === _goal) {
+            found = true;
+            break;
+        }
         const abled = getAbled(_i);
         if(abled.length) {
+            node = new Road(_i, node, abled);
             stack.push(...abled);
-            result.push(_i);
         }
         else {
-            stack.pop();
-            result.pop();
+            node = mapRoad.get(now.value);
         }
     }
-    if(stack.length) result.push(_goal);
+    if(found) return [...Road.toArr(node), _goal];
     else throw 'Not found.';
-    return result;
 };
+const mapRoad = new Map;
+class Road {
+    constructor(value, parent, children){
+        this.value = value;
+        this.parent = parent;
+        for(const i of children) mapRoad.set(i, this);
+    }
+    static toArr(node){
+        const arr = [];
+        while(node !== null) {
+            arr.unshift(node.value);
+            node = node.parent;
+        }
+        return arr;
+    }
+}
