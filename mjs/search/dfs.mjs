4b0dd345539ec2a8bd1b1ab5874e6a05ac290134
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-10-14 15:32:07 +0900

    Update dfs.mjs

--- mjs/search/dfs.mjs
+++ mjs/search/dfs.mjs
@@ -7,14 +7,16 @@ export const dfs = async ({maze, start, goal, width, height, update}) => {
     };
     const done = [...Array(width * height).fill(false)]; // 既に通った
     const getAbled = i => {
-        const [x, y] = toXY(i);
+        const [_x, _y] = toXY(i);
         return [
             [1, 0],
             [-1, 0],
             [0, 1],
             [0, -1]
-        ].flatMap(([_x, _y]) => {
-            const _i = toI(_x + x, _y + y);
+        ].flatMap(([a, b]) => {
+            const [x, y] = [_x + a, _y + b];
+            if(x < 0 || x >= width || y < 0 || y >= height) return [];
+            const _i = toI(x, y);
             return maze[_i] || done[_i] || stack.includes(_i) ? [] : [_i];
         });
     };
@@ -23,20 +25,20 @@ export const dfs = async ({maze, start, goal, width, height, update}) => {
           _goal = toI(...goal);
     while(stack.length) {
         const _i = stack.pop();
+        await update(_i);
         if(_i === _goal) break;
         done[_i] = true;
         const abled = getAbled(_i);
         if(abled.length) {
             stack.push(...abled);
             result.push(_i);
-            await update(_i);
         }
         else {
             stack.pop();
             result.pop();
         }
     }
-    if(result.length) result.push(_goal);
+    if(stack.length) result.push(_goal);
     else throw 'Not found.';
     return result;
 };
