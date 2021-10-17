d7800e9e3edc9e09b06bb3d541298605d2f85a14
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-10-17 14:14:48 +0900

    Update bfs.mjs

--- mjs/search/bfs.mjs
+++ mjs/search/bfs.mjs
@@ -20,37 +20,38 @@ export const bfs = async ({maze, start, goal, width, height, update}) => {
     const _start = toI(...start),
           _goal = toI(...goal),
           queue = [_start];
-    let node = null,
-        found = false;
+    let found = false;
     nodeMap.clear();
+    new Node(_start, null);
     while(queue.length) {
-        const _i = queue.shift();
-        node = nodeMap.has(_i) ? nodeMap.get(_i) : null;
+        const _i = queue.shift(),
+              node = nodeMap.get(_i);
         await update(_i);
         if(_i === _goal) {
             found = true;
             break;
         }
         const abled = getAbled(_i);
-        if(abled.length) {
-            new Node(_i, node, abled);
-            queue.push(...abled);
+        if(!abled.length) continue;
+        for(const i of abled) {
+            new Node(i, node);
+            queue.push(i);
         }
     }
-    if(found) return [...Node.toArr(node), _goal];
+    if(found) return Node.toArr(nodeMap.get(_goal));
     else throw 'Not found.';
 };
 const nodeMap = new Map;
 class Node {
-    constructor(value, parent, children){
-        this.value = value;
+    constructor(index, parent){
+        this.index = index;
         this.parent = parent;
-        for(const i of children) nodeMap.set(i, this);
+        nodeMap.set(index, this);
     }
     static toArr(node){
         const arr = [];
         while(node !== null) {
-            arr.unshift(node.value);
+            arr.unshift(node.index);
             node = node.parent;
         }
         return arr;
