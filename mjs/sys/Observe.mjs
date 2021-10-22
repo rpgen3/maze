845f0656396522d4244e73d56ba8aab95ef249a0
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-10-22 12:49:42 +0900

    Update Observe.mjs

--- mjs/sys/Observe.mjs
+++ mjs/sys/Observe.mjs
@@ -2,12 +2,12 @@ export class Observe {
     constructor(num){
         this.arr = [...Array(num)];
     }
-    unchanged(...arg){
-        let flag = true;
+    changed(...arg){
+        let flag = false;
         for(const [i, v] of this.arr.entries()) {
             if(v === arg[i]) continue;
             this.arr[i] = v;
-            if(flag) flag = false;
+            if(flag) flag = true;
         }
         return flag;
     }
