8a5ba161da5d7ce098d5d05aa39893c3831a3e21
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-10-26 12:56:51 +0900

    Update aStar.mjs

--- mjs/search/aStar.mjs
+++ mjs/search/aStar.mjs
@@ -1,24 +1,10 @@
+import {Heap} from 'https://rpgen3.github.io/maze/mjs/Heap.mjs';
 export const aStar = async ({maze, start, goal, width, height, update, heuristic, weight = [1, 1], giveup = false}) => {
     const toI = (x, y) => x + y * width;
     const toXY = i => {
         const x = i % width,
               y = i / width | 0;
         return [x, y];
-    };
-    const getMin = () => {
-        let _cost = Infinity,
-            _i = -1,
-            _node = null;
-        for(const [i, v] of openList.entries()) {
-            const node = nodeMap.get(v),
-                  {cost} = node;
-            if(_cost <= cost) continue;
-            _cost = cost;
-            _i = i;
-            _node = node;
-        }
-        openList.splice(_i, 1);
-        return _node;
     };
     const getAbled = i => {
         const [x, y] = toXY(i);
@@ -34,15 +20,16 @@ export const aStar = async ({maze, start, goal, width, height, update, heuristic
     };
     const _start = toI(...start),
           _goal = toI(...goal),
-          openList = [_start],
           [wG, wH] = weight,
-          calcH = i => heuristic(...goal, ...toXY(i)) * wH;
-    nodeMap.clear();
-    new Node(_start, null, 0, calcH(_start));
+          calcH = i => heuristic(...goal, ...toXY(i)) * wH,
+          nodeMap = new Map,
+          heap = new Heap();
+    nodeMap.set(_start, new Node(_start, null, 0, calcH(_start)));
+    heap.push(0, _start);
     let found = false,
         min = Infinity;
-    while(openList.length) {
-        const node = getMin(),
+    while(heap.list.length) {
+        const node = heap.pop(),
               {index, gCost, cost} = node;
         if(giveup) {
             if(min < cost) throw 'Not found.';
@@ -57,14 +44,14 @@ export const aStar = async ({maze, start, goal, width, height, update, heuristic
         if(!abled.length) continue;
         const g = gCost + 1;
         for(const i of abled) {
-            new Node(i, node, g * wG, calcH(i));
-            openList.push(i);
+            const tmp = new Node(i, node, g * wG, calcH(i));
+            nodeMap.set(i, tmp);
+            heap.push(tmp.cost, i);
         }
     }
     if(found) return Node.toArr(nodeMap.get(_goal));
     else throw 'Not found.';
 };
-const nodeMap = new Map;
 class Node {
     constructor(index, parent, gCost, hCost){
         this.index = index;
@@ -72,7 +59,6 @@ class Node {
         this.gCost = gCost;
         this.hCost = hCost;
         this.cost = gCost + hCost;
-        nodeMap.set(index, this);
     }
     static toArr(node){
         const arr = [];
